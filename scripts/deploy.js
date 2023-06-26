458a3322cdb251498b2da80b4cc1fc29ced492da
Sruttz <110161415+Sruttz@users.noreply.github.com>
2023-06-26 15:18:12 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,22 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  // Get the Points smart contract
-  const Degen = await hre.ethers.getContractFactory("DegenToken");
+  const Token = await hre.ethers.getContractFactory('Token');
+  const token = await Token.deploy();
 
-  // Deploy it
-  const degen = await Degen.deploy();
-  await degen.deployed();
+  await token.deploy();
 
-  // Display the contract address
-  console.log(`Degen token deployed to ${degen.address}`);
+  console.log("ERC20 contract deployed to: ", token.address);
+  
 }
 
-// Hardhat recommends this pattern to be able to use async/await everywhere
+// We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
   console.error(error);
